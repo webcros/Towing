78a45cb5e1667d87f8770173051af28b1647de74
Bidyadhar sahu <131295002+bidyadharsahu@users.noreply.github.com>
2026-08-20 18:59:37 +0530

    ds

---
+++ towing-aws-infra/test/towing-aws-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TowingAwsInfra from '../lib/towing-aws-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/towing-aws-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TowingAwsInfra.TowingAwsInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
